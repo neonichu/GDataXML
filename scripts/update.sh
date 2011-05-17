4c32d2d0a0d21c13329199ca5659cc760084a46e
NeoNacho <boris@icculus.org>
2011-05-17 22:21:55 +0200

    Initial source drop:
    * src/ and include/ subdirectories of Tidy without localize.c
    * GDataXML checkout from SVN
    * Checkout script for GDataXML, SF.net CVS for Tidy doesn't work
    * Initial Xcode project file

---
+++ scripts/update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# GDataXML (http://code.google.com/p/gdata-objectivec-client/)
+mkdir -p Sources/GDataXML &&
+svn co http://gdata-objectivec-client.googlecode.com/svn/trunk/Source/XMLSupport \
+		Sources/GDataXML
